9de7edfa5ff3015d272ee83c1094949a06beaf4d
simone <mac@mactekiMacBook-Air.local>
2015-10-15 09:49:56 +0800

    first commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function($) {
+	
+$('#pass').strength({
+            strengthClass: 'strength',
+            strengthMeterClass: 'strength_meter',
+            strengthButtonClass: 'button_strength',
+            strengthButtonText: 'Show Password',
+            strengthButtonTextToggle: 'Hide Password'
+        });
+
+});
